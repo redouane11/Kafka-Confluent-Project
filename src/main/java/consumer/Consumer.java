401a8b242334f4557997102363cd1e6aad51c14c
Redouane <red-key@hotmail.com>
2023-04-06 23:11:25 -0400

    Injecting a ConsumerRecord to get key and value

--- src/main/java/consumer/Consumer.java
+++ src/main/java/consumer/Consumer.java
@@ -1,13 +1,21 @@
 package consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.json.JSONObject;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer{
+    JSONObject jo = new JSONObject();
     @KafkaListener(topics = {"hobbit"}, groupId = "spring-boot-kafka")
-    public void consumer(String quote){
-        System.out.println("Received ------> : " + quote);
+    public void consumer(ConsumerRecord<Integer,String> record){
+        jo.put("value", record.value());
+        jo.put("key", record.key());
+        System.out.println("Received  ------> : " + jo);
+
+
+
     }
 }
 
